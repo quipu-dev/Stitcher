5b86d7fa09f7dd9cdf8d7855fb3c213ecc8f8eb3
doucx <doucxldh@gmail.com>
2026-01-10 19:44:10 +0800

    Capture: M store.py
    X-Quipu-Output-Tree: 4ea4232998eb56a84af230a7c6c6747b1fdded9a

--- snapshot/packages/stitcher-index/src/stitcher/index/store.py
+++ snapshot/packages/stitcher-index/src/stitcher/index/store.py
@@ -1,4 +1,4 @@
-from typing import Optional, List, Tuple
+from typing import Optional, List, Tuple, Set
 from .db import DatabaseManager
 from .types import FileRecord, SymbolRecord, ReferenceRecord
 
