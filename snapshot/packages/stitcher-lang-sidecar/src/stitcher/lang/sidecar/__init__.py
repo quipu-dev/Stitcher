19475039e0c17bc791f5e12d9645f80a98718944
doucx <doucxldh@gmail.com>
2026-01-14 18:15:55 +0800

    [WIP] feat: 将 Sidecar 提升为索引中的一等公民
    X-Quipu-Output-Tree: 3557fdf17c88f3dfeb7c89bc67797f30354ef569

--- snapshot/packages/stitcher-lang-sidecar/src/stitcher/lang/sidecar/__init__.py
+++ snapshot/packages/stitcher-lang-sidecar/src/stitcher/lang/sidecar/__init__.py
@@ -1,6 +1,8 @@
 __path__ = __import__("pkgutil").extend_path(__path__, __name__)
 
 from .adapter import SidecarAdapter
+from .indexer import SidecarIndexerAdapter
+from .uri import SidecarURIGenerator
 from .lock_manager import LockFileManager
 from .parser import parse_doc_references, parse_signature_references
 from .transform import SidecarTransformContext, SidecarTransformer
@@ -9,6 +11,8 @@ from .merger import DocstringMerger
 
 __all__ = [
     "SidecarAdapter",
+    "SidecarIndexerAdapter",
+    "SidecarURIGenerator",
     "LockFileManager",
     "parse_doc_references",
     "parse_signature_references",
